7cb6bd54cacb927afcdf2164df5d683ee9edbb8c
Daivik Goel <daivikgoel98@gmail.com>
2023-09-13 12:21:04 -0400

    Added HTTPS

--- src/index.js
+++ src/index.js
@@ -1,8 +1,22 @@
+const https = require('https');
+const fs = require('fs');
 const app = require('./app');
 
+// Load your SSL/TLS certificate and private key files
+const privateKey = fs.readFileSync('./SSL/private.key', 'utf8');
+const certificate = fs.readFileSync('./SSL/certificate.crt', 'utf8');
+
+const credentials = {
+  key: privateKey,
+  cert: certificate,
+};
+
 const port = process.env.PORT || 4000;
-app.listen(port, () => {
+
+const httpsServer = https.createServer(credentials, app);
+
+httpsServer.listen(port, () => {
   /* eslint-disable no-console */
-  console.log(`Listening: http://localhost:${port}`);
+  console.log(`Listening: https://localhost:${port}`);
   /* eslint-enable no-console */
 });
